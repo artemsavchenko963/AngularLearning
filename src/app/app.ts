fbf1f78ea441d900e9941b4edefbd6208ac77bd8
Artem <artemsavcenko963@gmail.com>
2025-11-04 22:13:49 +0100

    Components

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,22 +7,14 @@ import { SortDataPipe } from './pipes/sort-data-pipe';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, CutTextPipe, SortDataPipe],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  title = 'Learning Angular';
-  ageOfUsers = [{age: 18},{age: 13},{age: 20}]
-
-  constructor() {
-    setTimeout(() => {
-      this.ageOfUsers = [...this.ageOfUsers, {age: 16}];
-      console.log(this.ageOfUsers)
-    }, 2000);
-    setTimeout(() => {
-      this.ageOfUsers = [...this.ageOfUsers, {age: 35}];
-    }, 2000);
-
+  color = 'green';
+  checkCurrentValue(event: Event) {
+    const target = event.target as HTMLInputElement;
+    console.log('e', target.value);
   }
 }
