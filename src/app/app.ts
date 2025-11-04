27531003a7eaff084a98dfadcad9fcc5055d41ac
Artem <artemsavcenko963@gmail.com>
2025-11-04 20:50:25 +0100

    Create own pipes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,28 @@
-import { CommonModule, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { CommonModule, JsonPipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectorRef, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CutTextPipe } from './pipes/cut-text-pipe';
+import { SortDataPipe } from './pipes/sort-data-pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, CutTextPipe],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, CutTextPipe, SortDataPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   title = 'Learning Angular';
+  ageOfUsers = [{age: 18},{age: 13},{age: 20}]
+
+  constructor() {
+    setTimeout(() => {
+      this.ageOfUsers = [...this.ageOfUsers, {age: 16}];
+      console.log(this.ageOfUsers)
+    }, 2000);
+    setTimeout(() => {
+      this.ageOfUsers = [...this.ageOfUsers, {age: 35}];
+    }, 2000);
+
+  }
 }
