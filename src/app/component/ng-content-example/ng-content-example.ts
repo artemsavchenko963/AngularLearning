2db8314b252512d144c9d319d897aa2ea783d76e
Artem <artemsavcenko963@gmail.com>
2025-11-05 20:21:07 +0100

    Learn ng-content, select

---
+++ src/app/component/ng-content-example/ng-content-example.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-content-example',
+  imports: [],
+  templateUrl: './ng-content-example.html',
+  styleUrl: './ng-content-example.scss',
+})
+export class NgContentExample {
+
+}
