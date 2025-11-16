eb06292e11b182cbda04a1eacf1589b418b8aac8
Artem <artemsavcenko963@gmail.com>
2025-11-16 17:24:12 +0100

    Add Dynamic Components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
 import { CustomDirective } from './directives/custom-directive';
 import { RouterOutlet } from '@angular/router';
 import { Child } from './components/child/child';
@@ -6,16 +6,37 @@ import { CommonModule } from '@angular/common';
 import { NgContentExample } 
 from 
 './component/ng-content-example/ng-content-example';
+import { Dynamic } from './component/dynamic/dynamic';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Child, CommonModule,
-  NgContentExample, CustomDirective],
+  NgContentExample, CustomDirective, Dynamic],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
  toggler: boolean = true;
+
+ constructor(private viewContainer: ViewContainerRef) {
+
+ }
+
+showComponent() {
+  this.viewContainer.createComponent(Dynamic)
+} 
+
+@ViewChild('example', {read: ViewContainerRef}) container!: ViewContainerRef;
+ngAfterViewInit() {
+  const newComponent = this.container.createComponent(Dynamic)
+  // newComponent.instance.name = 'new name';
+  // newComponent.instance.callName();
+
+  newComponent.setInput('name', 'new name');
+  const someData = newComponent.instance.callName();
+  console.log('someData', someData);
+}
+
 }
 
 
